0bc2ec33010e5534cd67b86050d0a1b1cf2582a2
Alun035 <00001124856651sp@al.educacao.sp.gov.br>
2024-10-30 10:52:41 -0300

    Add files via upload
    Flashcards do Alura.

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Super Mario Bros Wonder.',
+    'Qual o nome do item principal das fases?',
+    'Wonder Seeds!'
+)
+
+criaCartao(
+    'Pokémon',
+    'Quais os tipos do pokémon Malamar?',
+    'Dark/Psíquico!'
+)
+
+criaCartao(
+    'Kirby And The Forgotten Land',
+    'Qual boss é um Tatu?',
+    'É o Sillydillo!'
+)
+
+criaCartao(
+    'Dragon Ball Sparking Zero',
+    'Quais personagens se desviam automaticamente quando estão parados?',
+    'Whis, e Goku (Ui/Mui)!'
+)
+
+criaCartao(
+    'Crash Bandicoot 4 Its About Time',
+    'Qual personagen vilão da franquia se torna jogável pela primeira vez?',
+    'É o Dingodille!'
+)
+
+criaCartao(
+    'Sonic Mania',
+    'Quais personagens são jogáveis no total (incluindo dlc)?',
+    'Sonic, Tails, Knuckles, Ray e Mighty!'
+)
